76f16c71ce01834c929a3b2d12ba5a6471f0b46a
Oren Richter <orenri@wismain.weizmann.ac.il>
2024-09-24 14:42:32 +0300

    implemented the binary search to find S+ values per S- and beta for the synapse count model

--- c_elegans_synapse_count_model_training.py
+++ c_elegans_synapse_count_model_training.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pickle
+from copy import deepcopy
 
-from c_elegans_independent_model_training import calc_elongation_factor
+from c_elegans_independent_model_training import calc_elongation_factor, convert_spls_dict_to_spls_across_dev, \
+    count_synapses_of_type
+from c_elegans_reciprocal_model_training import single_binary_search_update
 
 
 def p_synapse_is_formed(spls_across_dev, smi, beta, developmental_ages, time_passed_from_given_state,
@@ -61,3 +65,64 @@ def p_synapse_is_formed(spls_across_dev, smi, beta, developmental_ages, time_pas
 
     # The probability to have at least one synapse at the final step
     return num_synapses_pmf_across_time[-1, 1:].sum()
+
+
+def calc_expected_num_synapses_type_pair(neuronal_types_pair, smi, beta, reference_age, developmental_ages, spls,
+                                         data_path):
+    with open(data_path, 'rb') as f:
+        data = pickle.load(f)
+    sorted_neurons = sorted(data.nodes)
+    num_neurons = len(sorted_neurons)
+    expected_num_synapses = 0
+    spls_across_dev = convert_spls_dict_to_spls_across_dev(spls, neuronal_types_pair[0], neuronal_types_pair[1])
+    for pre_idx in range(num_neurons):
+        pre_neuron = sorted_neurons[pre_idx]
+        pre_type = data.nodes[pre_neuron]['type']
+        if neuronal_types_pair[0] != pre_type:
+            continue
+        for post_idx in range(num_neurons):
+            if pre_idx == post_idx:
+                continue
+            post_neuron = sorted_neurons[post_idx]
+            post_type = data.nodes[post_neuron]['type']
+            if neuronal_types_pair[1] != post_type:
+                continue
+            synapse_birth_time = max(data.nodes[pre_neuron]['birth_time'], data.nodes[post_neuron]['birth_time'])
+            if synapse_birth_time >= reference_age:
+                continue
+            synapse_age = reference_age - synapse_birth_time
+            norm_dist_between_neurons = np.linalg.norm(
+                data.nodes[pre_neuron]['coords'] - data.nodes[post_neuron]['coords'])
+
+            expected_num_synapses += p_synapse_is_formed(spls_across_dev, smi, beta, developmental_ages, synapse_age,
+                                                         reference_age, norm_dist_between_neurons)
+
+    return expected_num_synapses
+
+
+def search_spl_syn_count_model(neuronal_types_pair, smi, beta, reference_age, developmental_ages, spls,
+                               type_configuration, data_path, tolerance=0.05):
+    new_dev_stage = 0
+    while reference_age > developmental_ages[new_dev_stage]:
+        new_dev_stage += 1
+
+    spls_copy = deepcopy(spls)
+    cur_search_window = np.array([0, 1])
+    spls_copy[new_dev_stage][neuronal_types_pair] = (cur_search_window[1] + cur_search_window[0]) / 2
+    num_synapses_in_data = count_synapses_of_type(neuronal_types_pair, type_configuration, data_path=data_path)
+    expected_num_synapses_model = calc_expected_num_synapses_type_pair(neuronal_types_pair, smi, beta, reference_age,
+                                                                       developmental_ages, spls_copy, data_path)
+    iter = 0
+    while abs(expected_num_synapses_model - num_synapses_in_data) > tolerance:
+        spls_copy[new_dev_stage][neuronal_types_pair] = single_binary_search_update(num_synapses_in_data,
+                                                                                    expected_num_synapses_model,
+                                                                                    cur_search_window)
+        expected_num_synapses_model = calc_expected_num_synapses_type_pair(neuronal_types_pair, smi, beta,
+                                                                           reference_age,
+                                                                           developmental_ages, spls_copy, data_path)
+        if iter > 2 * 10 * np.log(10) / np.log(2):
+            # Probably the number of synapses can't be reached up to the tolerance with the given parameters, as the
+            # size of the square side is already smaller than 10e-10.
+            break
+        iter += 1
+    return spls_copy[new_dev_stage][neuronal_types_pair]
